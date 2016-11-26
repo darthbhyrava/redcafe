eccb880ad5363683f0bcdd7e4f57d856934c39cd
darthbhyrava <hbhyrava@gmail.com>
2016-11-27 05:29:32 +0530

    Add Beautiful Soup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # redcafe
+
 I'm curious. What can we do with millions of words on a football forum?
--- post-list.py
+++ post-list.py
@@ -1,11 +1,22 @@
 # A script to get data from a list of posts on a thread. 
 # Let's start with Juan Mata's thread.
+from bs4 import BeautifulSoup
 import urllib2
+
 proxy = urllib2.ProxyHandler({'http': 'proxy.iiit.ac.in:8080'})
 opener = urllib2.build_opener(proxy)
 urllib2.install_opener(opener)
+
 response = urllib2.urlopen('http://www.redcafe.net/threads/juan-mata-2016-17-performances.419643/page-8')
 data = response.read()
 file = open("mata8.html", 'w')
 file.write(data)
+file.close()
+
+with open("mata8.html") as fp:
+    soup = BeautifulSoup(fp)
+towrite = soup.find_all('blockquote')
+file = open('blockquotes', 'w')
+for i in towrite:
+    file.write(i.encode('utf-8')+"\n\n")
 file.close()
