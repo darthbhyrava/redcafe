f3484c3301f4d96da27dea4b38563ce5d96d72e1
darthbhyrava <hbhyrava@gmail.com>
2016-11-27 07:45:31 +0530

    All Cleaned Up

--- post-list.py
+++ post-list.py
@@ -3,20 +3,35 @@
 from bs4 import BeautifulSoup
 import urllib2
 
+def remover(el, tag='div', className=''):
+    if className:
+        for div in el.find_all(tag, { 'class' : className }): 
+            div.decompose()
+    else:
+        for div in el.find_all(tag): 
+            div.decompose()
+
 proxy = urllib2.ProxyHandler({'http': 'proxy.iiit.ac.in:8080'})
 opener = urllib2.build_opener(proxy)
 urllib2.install_opener(opener)
-
-response = urllib2.urlopen('http://www.redcafe.net/threads/juan-mata-2016-17-performances.419643/page-8')
+i = 1
+count = 0
+response = urllib2.urlopen('http://www.redcafe.net/threads/juan-mata-2015-16-performances.406616/page-{}'.format(i))
 data = response.read()
-file = open("mata8.html", 'w')
-file.write(data)
-file.close()
-
-with open("mata8.html") as fp:
-    soup = BeautifulSoup(fp)
+soup = BeautifulSoup(data)
 towrite = soup.find_all('blockquote')
 file = open('blockquotes', 'w')
+
 for i in towrite:
-    file.write(i.encode('utf-8')+"\n\n")
+    remover(i, 'div')
+    remover(i, 'br')
+    remover(i, 'blockquote', 'twitter-tweet')
+    x = i.contents
+    s = ''
+    for sentence in x:
+        s += str(sentence.encode('utf-8'))
+    if len(x):
+        count += 1
+        post = (str(count) + str('~~~') + "\t" + s.strip()).strip() + "\n\n"
+        file.write(post)
 file.close()
